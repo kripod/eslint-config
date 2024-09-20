bc3c8fd598638aba0547f2a1e19415e9923a9d31
Kristóf Poduszló <14854048+kripod@users.noreply.github.com>
2024-09-20 21:30:05 +0200

    feat: add 'node' config placeholder

---
+++ src/node.ts
@@ -0,0 +1,6 @@
+import type { Linter } from "eslint";
+import type { ESLintRules } from "eslint/rules";
+
+/* TODO: Adopt rules from 'eslint-plugin-n' */
+
+export default [] satisfies Linter.Config<ESLintRules>[];
