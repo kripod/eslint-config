d70513485acbf53e86853c5bb379efdbdfe96b21
Kristóf Poduszló <14854048+kripod@users.noreply.github.com>
2024-10-06 10:00:40 +0200

    revert: "refactor: use non-array configs"
    This reverts commit f114a466a04178cb0e7ddced20abf48fb989d55a.

--- src/node.ts
+++ src/node.ts
@@ -2,6 +2,6 @@ import type { Linter } from "eslint";
 
 /* TODO: Adopt rules from 'eslint-plugin-n' */
 
-const config: Linter.Config = {};
+const config: Linter.Config[] = [];
 
 export default config;
