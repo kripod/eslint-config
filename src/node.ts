0e5ba48792e7b790867a1a9acb1b63c173f1f2ea
Kristóf Poduszló <14854048+kripod@users.noreply.github.com>
2024-10-06 09:10:20 +0200

    refactor(node): remove unused `RulesRecord` narrowing

--- src/node.ts
+++ src/node.ts
@@ -1,8 +1,7 @@
 import type { Linter } from "eslint";
-import type { ESLintRules } from "eslint/rules";
 
 /* TODO: Adopt rules from 'eslint-plugin-n' */
 
-const config: Linter.Config<ESLintRules>[] = [];
+const config: Linter.Config[] = [];
 
 export default config;
