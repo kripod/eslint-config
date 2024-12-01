fc028ca17575e52935cb1d17c03a9a1cb8a1ef64
Kristóf Poduszló <14854048+kripod@users.noreply.github.com>
2024-12-01 12:58:32 +0100

    chore(eslint): refactor project config

--- eslint.config.js
+++ eslint.config.js
@@ -6,13 +6,18 @@ import typeCheckedConfig from "./dist/esm/type-checked.js";
 /** @type {import("eslint").Linter.Config[]} */
 export default [
   { files: ["**/*.jsx", "**/*.ts", "**/*.mts", "**/*.cts", "**/*.tsx"] },
+  ...baseConfig,
+  ...typeCheckedConfig,
   {
     languageOptions: {
       globals: {
         ...globals.node,
       },
+      parserOptions: {
+        projectService: {
+          allowDefaultProject: ["*.js"],
+        },
+      },
     },
   },
-  ...baseConfig,
-  ...typeCheckedConfig,
 ];
